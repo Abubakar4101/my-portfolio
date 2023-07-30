8a49804b9c8787c15b37f697efb84847f62dc525
M.Abubakar Siddique <abubakarsiddique4101@gmail.com>
2023-07-30 21:27:34 +0500

    portfolio added

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,43 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Head from "next/head";
 import Layout from "@/components/Layout";
 import AnimeHeading from "@/components/AnimeHeading";
 import Image from "next/image";
-import profilePic from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import formValidation from "../../public/images/projects/form-validation.jpg";
+import weatherApp from "../../public/images/projects/dynamic-weather.png";
+import wordpressTutorial from "../../public/images/projects/wordpress-tutorial.png";
 import Link from "next/link";
 import {motion} from "framer-motion";
 import { GithubIcon, LinkArrow} from "@/components/Icons";
 import TransitionPage from "@/components/TransitionPage";
 
-const ProjectContainer = ({thumbnail, typeOfProject, title, detail, gitLink, liveLink, containerClass, isHovered}) => {
+
+const ProjectContainer = ({
+                              thumbnail,
+                              typeOfProject,
+                              title,
+                              detail,
+                              gitLink,
+                              liveLink,
+                              containerClass,
+                              gitBtn,
+                              liveBtn,
+                              typeOfLiveBtn,
+                              isHovered,
+                              detailPara
+                        }) => {
     return(
         <motion.div className=" bg-dark dark:bg-light rounded-3xl translate-x-3 translate-y-3" initial={{scale: 0}} whileInView={{scale: 1, transition: {duration: 1} }} viewport={{once: true}}>
             <motion.div className={`flex flex-col  gap-5 md:p-5 ${containerClass} border-2 border-solid border-dark dark:border-light rounded-3xl p-5 bg-light dark:bg-dark -translate-x-3 -translate-y-3`} whileHover={{transform: "translate(0rem, 0rem)"}}>
                 {/*<Image src={thumbnail} alt="Abubakar" className={`rounded-3xl bg-light ${imgClass} hover:cursor-pointer`} priority={false}/>*/}
                 <motion.div whileHover={{scale: 1.02}}>
-                    <Image src={thumbnail} alt="Abubakar" className={`rounded-3xl h-full bg-light dark:bg-dark hover:cursor-pointer`} priority/>
+                    <Image src={thumbnail} alt="Abubakar" className={`rounded-3xl h-full bg-light dark:bg-dark hover:cursor-pointer`} priority={true}/>
                 </motion.div>
                 <div className="flex flex-col gap-3">
-                                    <span className="text-lg text-cyan-700 dark:text-blue-400 font-medium">
-                                        {typeOfProject}
-                                    </span>
+                    <span className="text-lg text-cyan-700 dark:text-blue-400 font-medium">
+                        {typeOfProject}
+                    </span>
                     <h3 className="font-bold text-dark dark:text-light text-xl md:text-3xl">
-                        {title}
+                        {title}<br />
                     </h3>
-                    <p className="font-medium h-full whitespace-normal text-dark dark:text-light">
+                    <p className={`font-medium ${detailPara}  h-full whitespace-normal text-dark dark:text-light`}>
                         {detail}
                     </p>
                     <div className="flex items-center justify-center md:justify-start flex-col md:flex-row gap-3">
-                        <Link href={gitLink} target={"_blank"} className="btn btn-neutral dark:text-dark dark:bg-light dark:border-light w-full md:w-auto"
+                        <Link href={gitLink} target={"_blank"} className={`btn btn-neutral ${gitBtn} dark:text-dark dark:bg-light dark:border-light w-full md:w-auto`}
                               onMouseOver={() => isHovered("hovered")} onMouseLeave={() => isHovered("default")}
                         >
                             GitHub
                             <GithubIcon className="w-5 h-5" />
                         </Link>
-                        <Link href={liveLink} target={"_blank"} className="btn btn-accent dark:btn-primary w-full md:w-auto"
+                        <Link href={liveLink} target={"_blank"} className={`btn btn-accent ${liveBtn} dark:btn-primary w-full md:w-auto`}
                               onMouseOver={() => isHovered("hovered")} onMouseLeave={() => isHovered("default")}
                         >
-                            Live
+                            {typeOfLiveBtn}
                             <LinkArrow className="dark:text-light"/>
                         </Link>
                     </div>
@@ -64,33 +80,39 @@ const Projects = ({isHovered}) => {
                     <div className="mt-14 mb-14 flex flex-col gap-16">
 
                         <ProjectContainer
-                            thumbnail={profilePic}
+                            thumbnail={weatherApp}
                             typeOfProject={"Featured Project"}
-                            title={"Crypto Screener Application"}
-                            detail={"A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."}
-                            gitLink={"/"}
+                            title={"Dynamic Weather App"}
+                            detail={"A dynamic weather app provides users with a seamless and intuitive experience for accessing real-time weather data. This dynamic weather app is a web app that uses the Open Weather API to fetch real-time weather data for any location. The app is built using HTML5, CSS5, and Node JS, and it can be used to display the current weather conditions, forecast, and other weather-related information."}
+                            gitLink={"https://github.com/Abubakar4101/Dynamic-Weather-App"}
                             liveLink={"/"}
                             containerClass={"lg:flex-row"}
-                            imgClass={"p-5"}
+                            gitBtn={"flex"}
+                            liveBtn={"hidden"}
+                            typeOfLiveBtn={"Live"}
                             isHovered={isHovered}
                         />
                         <div className="flex flex-col lg:flex-row gap-16">
                             <ProjectContainer
-                                thumbnail={profilePic}
-                                typeOfProject={"Featured Project"}
-                                title={"Crypto Screener Application"}
-                                detail={"A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."}
+                                thumbnail={wordpressTutorial}
+                                typeOfProject={"Video"}
+                                title={"WordPress Tutorial for Beginner"}
                                 gitLink={"/"}
-                                liveLink={"/"}
+                                liveLink={"https://www.youtube.com/watch?v=0yijB5xObKU&ab_channel=AbTechWorld"}
+                                gitBtn={"hidden"}
+                                liveBtn={"relative"}
+                                typeOfLiveBtn={"Watch"}
                                 isHovered={isHovered}
+                                detailPara={"hidden"}
                             />
                             <ProjectContainer
-                                thumbnail={profilePic}
-                                typeOfProject={"Featured Project"}
-                                title={"Crypto Screener Application"}
-                                detail={"A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."}
-                                gitLink={"/"}
+                                thumbnail={formValidation}
+                                typeOfProject={"Feature"}
+                                title={"Form Validation Using JavaScript"}
+                                gitLink={"https://github.com/Abubakar4101/Form-Validation-using-JS"}
                                 liveLink={"/"}
+                                liveBtn={"hidden"}
+                                detailPara={"hidden"}
                                 isHovered={isHovered}
                             />
                         </div>
