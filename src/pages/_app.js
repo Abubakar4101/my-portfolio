708468f8d42b0688e5b7303e63548ea898a7fa15
M.Abubakar Siddique <abubakarsiddique4101@gmail.com>
2023-07-30 14:22:28 +0500

    portfolio added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,7 @@ export default function MyApp({ Component, pageProps }) {
       <>
           <Head>
               <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <link rel="icon" href="/favicon.ico" />
+              <link rel="icon" href="/favicon.ico" className="rounded-full"/>
           </Head>
           <ThemeProvider enableSystem={true} attribute="class">
               <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
